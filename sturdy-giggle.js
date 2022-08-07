634d2f38d06c7fe75ba647e0c52d63df84091ddd
RPE <vitali.shuman96@gmail.com>
2022-08-07 20:11:35 +0300

    Add initial (not working) logic for drawing column
    Add function that calculates width for all columns and return it in useful represenation.
    Add iniitial structure of column drawing functions

--- sturdy-giggle.js
+++ sturdy-giggle.js
@@ -1,5 +1,9 @@
 // TODO: 	Add treshold
 //  			Add rowHeightUnits
+// 				Move all this. props to constructor
+// 				Instead of filter use Binary search
+// 				rendered* (renderedInfo) -> rendering
+//  			clear vars on render
 
 class Sturdy {
 	constructor({ container, rowHeight, rowCount, columnsCount, cellRenderer, columns } = {}) {
@@ -13,7 +17,7 @@ class Sturdy {
 
 		this.treshold = 0;
 		this.rowsTreshold = 0;
-		this.columnsTreshold = 0;
+		this.columnsTreshold = 1;
 
 		this.rowsInViewport = Math.floor(this.containerHeight / this.rowHeight);
 
@@ -23,14 +27,15 @@ class Sturdy {
 		this.columns = columns;
 		this.columnsCount = this.columns.length;
 
-		this.currentLeft = 0;
-		this.currentTop = 0;
-
 		this.firstVisibleRowIndex = 0;
+		this.columnRenderingTolerance = 400;
+		this.currentRenderedArea = [0, 0];
 
 		this.cellsMap = new Map();
+		this.renderedColumnsMap = new Map();
 
 		this.scrollTop = 0;
+		this.scrollLeft = 0;
 	}
 
 	throttleFunction(func, ms) {
@@ -61,38 +66,92 @@ class Sturdy {
 		return wrapper;
 	}
 
+	computeColumnsRenderInfo(columns) {
+		let totalWidth = 0;
+		const renderedInfo = columns.reduce((acc, curr, i) => {
+			let obj = {
+				start: totalWidth,
+				end: totalWidth + curr.width,
+				column: curr,
+			};
+			totalWidth += curr.width;
+			acc.push(obj);
+			return acc;
+		}, []);
+
+		const renderedInfoMap = new Map();
+		const renderedInfoKeys = [];
+		renderedInfo.forEach((info) => {
+			renderedInfoMap.set(info.start, info);
+			renderedInfoKeys.push(info.start);
+		});
+
+		return {
+			renderedInfo,
+			renderedInfoMap,
+			renderedInfoKeys,
+			totalWidth,
+		};
+	}
+
+	renderColumn(column) {
+		if (rowIndex < 0 || rowIndex >= this.rowCount) {
+			return;
+		}
+		const columns = columnsToRender || this.columns;
+		let leftPadding = this.currentLeft;
+		let topPadding = this.rowHeight * rowIndex;
+		let row = [];
+		columns.forEach((column, columnIndex) => {
+			let inlineStyle = `height:${this.rowHeight}px;max-width:${column.width}px ;left: ${leftPadding}${column.widthUnits}; top:${topPadding}px; position: absolute; display: flex; justify-content: center; align-items: center;`;
+			const renderedCell = this.cellRenderer({ rowIndex, columnIndex, column, inlineStyle });
+
+			row.push(renderedCell);
+			leftPadding += column.width;
+		});
+
+		this.container.append(...row);
+
+		return { row };
+	}
+
 	render() {
 		const container = this.container;
+		const { renderedInfo, renderedInfoMap, renderedInfoKeys, totalWidth } = this.computeColumnsRenderInfo(this.columns);
+		this.renderedInfo = renderedInfo;
+		this.renderedInfoMap = renderedInfoMap;
+		this.renderedInfoKeys = renderedInfoKeys;
+		this.totalWidth = totalWidth;
+
+		this.cellsMap = new Map();
+		this.firstVisibleRowIndex = 0;
+
+		container.style.width = totalWidth;
 		container.style.height = this.rowHeight * this.rowCount + "px";
-		container.style.width =
-			this.columns.reduce((width, column) => {
-				width += column.width;
-				return width;
-			}, 0) + "px";
 
-		const columnsToRender = [];
-		let freeWidth = this.containerWidth;
-		for (let i = 0; i < this.columns.length; i++) {
-			if (freeWidth > 0) {
-				columnsToRender.push(this.columns[i]);
-				freeWidth -= this.columns[i].width;
-			} else {
-				break;
-			}
-		}
-
-		// const throttledScroll = this.throttleFunction(, 170);
 		const throttledScroll = this.throttleFunction(this.onScroll.bind(this), 17);
 		container.parentNode.addEventListener("scroll", throttledScroll);
-		this.cellsMap = new Map();
-		for (let i = 0; i < this.rowsInViewport + this.treshold; i++) {
-			const rendered = this.renderRow(i, columnsToRender);
 
-			this.cellsMap.set(i, rendered.row);
-		}
+		this.setScrollLeft(0);
+		this.setScrollTop(0);
 
-		console.table(this.cellsMap);
-		this.firstVisibleRowIndex = 0;
+		// const columnsToRender = [];
+		// let freeWidth = this.containerWidth;
+		// for (let i = 0; i < this.columns.length; i++) {
+		// 	if (freeWidth > 0) {
+		// 		columnsToRender.push(this.columns[i]);
+		// 		freeWidth -= this.columns[i].width;
+		// 	} else {
+		// 		break;
+		// 	}
+		// }
+
+		// for (let i = 0; i < this.rowsInViewport + this.treshold; i++) {
+		// 	const rendered = this.renderRow(i, columnsToRender);
+		// 	this.cellsMap.set(i, rendered.row);
+		// }
+
+		// console.table(this.cellsMap);
 	}
 
 	onScroll(event) {
@@ -103,8 +162,8 @@ class Sturdy {
 		}
 
 		const newLeftScroll = event.target.scrollLeft;
-		if (newLeftScroll !== this.scrollTop) {
-			console.log("Left");
+		if (newLeftScroll !== this.scrollLeft) {
+			this.setScrollLeft(newLeftScroll);
 			return;
 		}
 	}
@@ -182,7 +241,43 @@ class Sturdy {
 		// }
 	}
 
-	renderColumn(column) {}
+	setScrollLeft(scrollLeft) {
+		if (scrollLeft >= this.currentRenderedArea[1] || scrollLeft + this.containerWidth <= this.currentRenderedArea[0]) {
+			console.log("full hor");
+			this._fullRedrawOnScrollLeft(scrollLeft);
+		} else if (scrollLeft - this.currentRenderedArea[0] <= this.columnRenderingStartPoint) {
+			console.log("left hor");
+			this._partialRedrawOnScrollLeftLeft(scrollLeft);
+		} else if (this.currentRenderedArea[1] - scrollLeft <= this.columnRenderingStartPoint) {
+			console.log("right hor");
+			this._partialRedrawOnScrollLeftRight(scrollLeft);
+		}
+	}
+
+	_fullRedrawOnScrollLeft(scrollLeft) {
+		scrollLeft = 1680;
+		this.cellsMap = new Map();
+		this.renderedColumnsMap = new Map();
+		this.container.replaceChildren();
+
+		let startColumnIndex = this.renderedInfoKeys;
+		for (let i = 1, prev = this.renderedInfoKeys?.[0]; i < this.renderedInfoKeys.length; i++) {
+			const curr = this.renderedInfoKeys[i];
+			if (prev <= scrollLeft && scrollLeft <= curr) {
+				startColumnIndex = prev;
+				break;
+			}
+			prev = curr;
+		}
+
+		console.log(startColumnIndex);
+		// console.table(this.renderedInfo);
+		// console.table(this.renderedInfoMap);
+		console.log(this.renderedInfoKeys);
+		// console.table(this.totalWidth);
+
+		// this.firstVisibleRowIndex
+	}
 
 	removeRow(rowIndex) {}
 
